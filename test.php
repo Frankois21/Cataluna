9c8c20ca8cfb212973752f4f03d0be781b7e6b92
Fanny-Jan <fanny.jan12@gmail.com>
2017-03-08 17:29:32 +0100

    trucs

--- test.php
+++ test.php
@@ -1,45 +1,95 @@
+
 <?php
 
+include 'header.php';
+include 'connect.php';
 
-                $id = $postClean['id'];
+$bdd = mysqli_connect(SERVER, USER, PASS, DB);
 
-
-            if ($id) {
-                $req = "UPDATE pizza SET nom='$nom',
-                                          ingredients='$ingredients',
-                                          base='$base',
-                                          petit_prix='$petit_prix',
-                                          grand_prix='$grand_prix',
-                                          cache='$cache'
-                                      WHERE id=$id";
-                echo $req;
-
-                value="'.$nom.'"
-
-            if (mysqli_query($bdd, $req)) {
-                header('Location: admin.php');
-            }
+mysqli_set_charset($bdd,"utf8");
 
 
+$req = "SELECT id, nom, ingredients, base, petit_prix, grand_prix 
+            FROM pizza";
+$res = mysqli_query($bdd, $req);
 
 
+echo '<div class="ancre hidden-xs" id="tomate"></div>
+<div class="titre_menu">
+    <h2>Nos bases tomate</h2>
+    <p>Disponibles en 28cm et 33cm</p>
+</div>';
 
 
-                               $cache = $postClean['cache'];
-                cache='$cache'
+echo '<div class="row">';
+    while($data = mysqli_fetch_assoc($res)) {
+        if ($data['base'] == 'tomate') {
+            echo '
 
-                    <div class="form-group">
-        <div class="col-sm-offset-2 col-sm-10">
-            <div class="checkbox">
-                <label>
-                    <input type="checkbox" name="cache" value="1"> Cacher :)
-                </label>
-            </div>
+    <div class="col-sm-6 col-md-4 col-lg-2 pizza">
+        <div class="text-center">
+            <h3>' . $data['nom'] . '</h3>
+            <p>' . $data['ingredients'] . '</p>
+            <p class="prix">' . $data['petit_prix'] . ' €  - ' . $data['grand_prix'] . ' €</p>
         </div>
-    </div>
+    </div>';
+        }
+    }
+echo '
+</div>
+</div>';
 
 
-                          <form method="POST" action="deleteEleve.php">
-            <input type="hidden" name="id" value="'.$data['id'].'"/>
-            <input  class="btn btn-danger" type="submit" value="Delete" name="delete"/>
-          </form>
+echo '<div class="ancre hidden-xs" id="creme"></div>
+<div class="titre_menu">
+    <h2>Nos bases creme</h2>
+    <p>Disponibles en 28cm et 33cm</p>
+</div>';
+
+
+echo '<div class="row">';
+while($data = mysqli_fetch_assoc($res)) {
+    if ($data['base'] == 'creme') {
+        echo '
+
+    <div class="col-sm-6 col-md-4 col-lg-4 pizza">
+        <div class="text-center">
+            <h3>' . $data['nom'] . '</h3>
+            <p>' . $data['ingredients'] . '</p>
+            <p class="prix">' . $data['petit_prix'] . ' €  - ' . $data['grand_prix'] . ' €</p>
+        </div>
+    </div>';
+    }
+}
+echo '
+</div>
+</div>';
+
+echo '<div class="ancre hidden-xs" id="dessert"></div>
+<div class="titre_menu">
+    <h2>Nos desserts</h2>
+    <p>Disponibles en 28cm et 33cm</p>
+</div>';
+
+
+echo '<div class="row">';
+while($data = mysqli_fetch_assoc($res)) {
+    if ($data['base'] == 'dessert') {
+        echo '
+
+    <div class="col-sm-6 col-md-4 col-lg-4 pizza">
+        <div class="text-center">
+            <h3>' . $data['nom'] . '</h3>
+            <p>' . $data['ingredients'] . '</p>
+            <p class="prix">' . $data['petit_prix'] . ' €  - ' . $data['grand_prix'] . ' €</p>
+        </div>
+    </div>';
+    }
+}
+echo '
+</div>
+</div>';
+
+
+    include 'footer.php';
+?>
